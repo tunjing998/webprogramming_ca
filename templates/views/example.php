589bb40e075cb8ac6568b3738cc2a2de8fa3ed04
Shane <shane+gitkraken@nodehead.com>
2019-01-26 17:52:24 +0000

    Fixes typos in non-functional components

--- templates/views/example.php
+++ templates/views/example.php
@@ -1,2 +1,2 @@
 <h1>Example</h1>
-<p>This is just an example of a view using $local value. Here is is: <?= $locals['someLocalKey'] ?></p>
+<p>This is just an example of a view using a $local value. Here it is: <?= $locals['someLocalKey'] ?></p>

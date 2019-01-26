acdf6aed6388b01bcd91bdb0ebc3813a74a8bcaf
Shane <shane+gitkraken@nodehead.com>
2019-01-26 14:43:00 +0000

    Add a stub file for the Rapid library
    This stub file includes comments to block out the main classes that
    will be needed for the library, and adds default constructors for
    the these classes that will throw an exception is called (indicating
    that they have not been implemented yet)

---
+++ lib/Rapid.php
@@ -0,0 +1,89 @@
+<?php namespace Rapid; ?>
+<?php
+
+  /**
+   * This is the Rapid MVC framework created for GD2/CO2a
+   * Web Development (2018/19) CA3. This version has no
+   * in-built support or helpers for Models, so it is really
+   * just a VC framework (we'll add models later).
+   *
+   * Implementation details are described in comments in
+   * this file. Usage details will be documented in a
+   * README.md file in the root of this project.
+   *
+   * To limit the number of requires needed per request,
+   * all of this library in included in this one file.
+   *
+   * This library makes use of a namespace. To access the
+   * classes in this library, you must reference their
+   * namespace:
+   *    Wont' work: new Request();
+   *    Will work : new \Rapid\Request();
+   * Lear more about namespaces at:
+   *    https://php.net/manual/en/language.namespaces.php
+   */
+
+  ##########################################################
+  #### !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! #####
+  #### !!! YOU SHOULD NEVER NEED TO EDIT THIS FILE !!! #####
+  #### !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! #####
+  ##########################################################
+
+
+  ##########################################################
+  # Exceptions
+  ##########################################################
+
+
+  ##########################################################
+  # Request
+  ##########################################################
+
+  class Request {
+
+    public function __construct() {
+      throw new Exception('Request class has not yet been implemented');
+    }
+
+  }
+
+
+  ##########################################################
+  # Response
+  ##########################################################
+
+  class Response {
+
+    public function __construct() {
+      throw new Exception('Response class has not yet been implemented');
+    }
+
+  }
+
+
+  ##########################################################
+  # Renderer
+  ##########################################################
+
+  class Renderer {
+
+    public function __construct() {
+      throw new Exception('Renderer class has not yet been implemented');
+    }
+
+  }
+
+
+  ##########################################################
+  # Router
+  ##########################################################
+
+  class Router {
+
+    public function __construct() {
+      throw new Exception('Router class has not yet been implemented');
+    }
+
+  }
+
+?>

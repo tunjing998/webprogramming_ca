e68b99d6294a02ac8d6f6a80fc35fc3900075967
TunJing Ang <tunjing998@gmail.com>
2019-05-01 20:40:17 +0100

    Add add to cart button
    Add 'add to cart' button to home page and product page

--- templates/views/home.php
+++ templates/views/home.php
@@ -6,6 +6,9 @@
                     <p>Name  : <?= $product->getProductName() ?></p>
                     <p>Price :<?= $product->getProductPrice() ?></p>
                     <a href='<?= BASE_DIR ?>/product/<?= $product->getProductId() ?>'>More Details </a>
+                    <input type="hidden" id='product-name<?= $product->getProductId() ?>' value='<?= $product->getProductName() ?>'>
+                    <input type="hidden" id='product-price<?= $product->getProductId() ?>' value='<?= $product->getProductPrice() ?>'>
+                    <button class='item' value='<?= $product->getProductId() ?>'>Add to Cart</button>
             </div>
         <?php } ?>
     </div>
--- templates/views/product.php
+++ templates/views/product.php
@@ -42,9 +42,12 @@
                 <?php foreach ($locals['products'] as $product) { ?>
                     <div class="col-3 m-3 shadow bg-black border rounded product-item">
                         <img src="<?= IMG_DIR . $product->getProductImageAddr() ?>" width="250px" height="250px">
-                        <p>Name  : <?= $product->getProductName() ?></p>
+                        <p>Name : <?= $product->getProductName() ?></p>
                         <p>Price : <?= $product->getProductPrice() ?></p>
                         <a href='<?= BASE_DIR ?>/product/<?= $product->getProductId() ?>'>More Details </a>
+                        <input type="hidden" id='product-name<?= $product->getProductId() ?>' value='<?= $product->getProductName() ?>'>
+                        <input type="hidden" id='product-price<?= $product->getProductId() ?>' value='<?= $product->getProductPrice() ?>'>
+                        <button class='item' value='<?= $product->getProductId() ?>'>Add to Cart</button>
                     </div>
                 <?php } ?>
             </div>
